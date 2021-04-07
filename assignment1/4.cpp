975e8b408c4e8dfdb39a30e4cb8a874f09307e94
hrahul2605 <hrahul2605@gmail.com>
2021-04-07 23:17:23 +0530

    result for 4

--- assignment1/4.cpp
+++ assignment1/4.cpp
@@ -43,7 +43,9 @@ int main()
 {
     string writeFile = "large_data_set.txt";
     ofstream FILE;
-    FILE.open("result_4.txt");
+    FILE.open("Result4_UD.csv");
+
+    FILE << "Total Run, Time Taken, No. of operations, Average Time Taken, Average No Operations\n";
 
     long long int index = 2;
 
@@ -59,64 +61,96 @@ int main()
         double timeTaken = 0.0;
         timeTaken = runSort<int>(writeFile.c_str(), count);
 
-        FILE << "Time Taken for sorting UD Data set using Randomized Quick Sort, ";
-        FILE << " 1 run ";
+        // FILE << "Time Taken for sorting UD Data set using Randomized Quick Sort, ";
+        // FILE << " 1 run ";
+        FILE << "1,";
         FILE << timeTaken;
-        FILE << " seconds ";
-        FILE << "No. of operations: ";
+        FILE << ",";
+        // FILE << " seconds ";
+        // FILE << "No. of operations: ";
         FILE << count;
+        FILE << ",,";
         FILE << "\n";
 
         totalCount += count;
         totalTimeTaken += timeTaken;
     }
 
-    FILE << "\n\nTime Taken for sorting UD Data set using Randomized Quick Sort, ";
+    // FILE << "\n\nTime Taken for sorting UD Data set using Randomized Quick Sort, ";
     FILE << TOTAL_RUNS;
-    FILE << " runs ";
+    FILE << ",";
+    // FILE << " runs ";
+    FILE << ",,";
     FILE << totalTimeTaken / TOTAL_RUNS;
-    FILE << " seconds ";
-    FILE << "Avg. No. of operations: ";
+    // FILE << " seconds ";
+    // FILE << "Avg. No. of operations: ";
+    FILE << ",";
     FILE << totalCount / TOTAL_RUNS;
-    FILE << "\n\n";
+    // FILE << "\n\n";
+    FILE << "\n";
+
+    FILE.close();
+    FILE.open("Result4_ND.csv");
+    FILE << "Total Run, Time Taken, No. of operations, Average Time Taken, Average No Operations\n";
 
     totalCount = 0;
     totalTimeTaken = 0.0;
 
-
-    cout << "Generating Random Numbers from Normal Distribution.\n";
-    generateNDAndWriteToFile(writeFile.c_str());
-
     for (int i = 0; i < TOTAL_RUNS; i++)
     {
-        cout << "Generating Random Numbers from Uniform Distribution.\n";
-        generateUDAndWriteToFile(writeFile.c_str());
+        cout << "Generating Random Numbers from Normal Distribution.\n";
+        generateNDAndWriteToFile(writeFile.c_str());
 
         long long int count = 0;
         double timeTaken = 0.0;
         timeTaken = runSort<float>(writeFile.c_str(), count);
 
-        FILE << "Time Taken for sorting ND Data set using Randomized Quick Sort, ";
-        FILE << " 1 run ";
+        // FILE << "Time Taken for sorting ND Data set using Randomized Quick Sort, ";
+        // FILE << " 1 run ";
+        // FILE << timeTaken;
+        // FILE << " seconds ";
+        // FILE << "No. of operations: ";
+        // FILE << count;
+        // FILE << "\n";
+
+        // FILE << "Time Taken for sorting UD Data set using Randomized Quick Sort, ";
+        // FILE << " 1 run ";
+        FILE << "1,";
         FILE << timeTaken;
-        FILE << " seconds ";
-        FILE << "No. of operations: ";
+        FILE << ",";
+        // FILE << " seconds ";
+        // FILE << "No. of operations: ";
         FILE << count;
+        FILE << ",,";
         FILE << "\n";
 
         totalCount += count;
         totalTimeTaken += timeTaken;
     }
 
+    // FILE << "\n\nTime Taken for sorting ND Data set using Randomized Quick Sort, ";
+    // FILE << TOTAL_RUNS;
+    // FILE << " runs ";
+    // FILE << totalTimeTaken / TOTAL_RUNS;
+    // FILE << " seconds ";
+    // FILE << "Avg. No. of operations: ";
+    // FILE << totalCount / TOTAL_RUNS;
+    // FILE << "\n\n";
 
-    FILE << "\n\nTime Taken for sorting ND Data set using Randomized Quick Sort, ";
+    // FILE << "\n\nTime Taken for sorting UD Data set using Randomized Quick Sort, ";
     FILE << TOTAL_RUNS;
-    FILE << " runs ";
+    FILE << ",";
+    // FILE << " runs ";
+    FILE << ",,";
     FILE << totalTimeTaken / TOTAL_RUNS;
-    FILE << " seconds ";
-    FILE << "Avg. No. of operations: ";
+    // FILE << " seconds ";
+    // FILE << "Avg. No. of operations: ";
+    FILE << ",";
     FILE << totalCount / TOTAL_RUNS;
-    FILE << "\n\n";
+    // FILE << "\n\n";
+    FILE << "\n";
+
+    FILE.close();
 
     return 0;
 }
